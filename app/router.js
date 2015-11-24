e98a302dfb18007a33bd7dcf738eed2bc8f3ecdd
Kevin Lee <kevinwarrenlee@gmail.com>
2015-11-23 20:08:30 -0600

    added strava stub

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('activity', function() {});
+  this.route('activity', function() {
+    this.route('strava');
+  });
 });
 
 export default Router;
