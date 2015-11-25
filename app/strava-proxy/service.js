da2688da0a3854773e8d2f81e9975cab38553c1c
Kevin Lee <kevinwarrenlee@gmail.com>
2015-11-24 20:56:16 -0600

    wired up strava history table

--- app/strava-proxy/service.js
+++ app/strava-proxy/service.js
@@ -1,24 +1,43 @@
 import Ember from 'ember';
 import ENV from 'runnersq/config/environment';
 
+var cachedActivities;
+
 export default Ember.Service.extend({
   getActivities(perPage) {
-    console.log(ENV.STRAVA_ACCESS_TOKEN);
-    console.log(ENV.STRAVA_API_URL);
     return new Ember.RSVP.Promise(function(resolve) {
-      Ember.$.ajax({
-        url: ENV.STRAVA_API_URL,
-        type: 'GET',
-        dataType: 'jsonp',
-        data: {
-          access_token: ENV.STRAVA_ACCESS_TOKEN,
-          per_page: 200
-        }
-      }).done((res)=>{
-        console.log('SUCCESS: ', res);
-      }, (error)=>{
-        console.log('ERROR: ',error);
-      });
+      if (cachedActivities) {
+        console.log("from cache");
+        resolve(cachedActivities);
+      } else {
+        Ember.$.ajax({
+          url: ENV.STRAVA_API_URL,
+          type: 'GET',
+          dataType: 'jsonp',
+          data: {
+            access_token: ENV.STRAVA_ACCESS_TOKEN,
+            per_page: 200
+          }
+        }).then((response)=>{
+          var activities = [];
+          $.each(response, function( index, value ) {
+            var activity = {
+              id: value.id,
+              type: value.type,
+              distance: value.distance,
+              duration: value.moving_time,
+              start_time:value.start_date_local,
+              source:'Strava'
+            }
+            activities.push(activity);
+          });
+          cachedActivities = activities;
+          resolve(cachedActivities);
+
+        }, (error)=>{
+          console.log('ERROR: ',error);
+        });
+      }
     })
   }
 });
