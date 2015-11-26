4c4989c4909e1d3d1e87c517ee2cace51d1fc3ca
Kevin Lee <kevinwarrenlee@gmail.com>
2015-11-26 07:44:25 -0600

    week is now displayed right :)

--- app/index/route.js
+++ app/index/route.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
+// import moment from 'moment';
 
 export default Ember.Route.extend({
   mapReduceSum(items, property){
     let mappedItems = items.mapBy(property);
     let reducedItems = mappedItems.reduce( (prev, curr) => prev + curr );
     return reducedItems;
+  },
+  durationForDisplay(totalSeconds){
+    let d = moment.duration(totalSeconds, 'seconds');
+    let durationDisplay = d.hours() + 'h ' + d.minutes() + 'm';
+    return durationDisplay;
   },
   historyStore: Ember.inject.service(),
   model: function(){
@@ -13,16 +19,16 @@ export default Ember.Route.extend({
 
     const summaryPromise = new Ember.RSVP.Promise((resolve)=> {
       this.get('historyStore').getActivities().then((activities) => {
-        let thisWeeksActivities = activities.filter(function(value) {
+        let summayActivities = activities.filter(function(value) {
           return moment.utc(value.start_time).isAfter(moment().day(-1));
         });
 
-        let totalMeters = mapReduce(thisWeeksActivities, 'distance');
+        let totalMeters = mapReduce(summayActivities, 'distance');
         let totalMiles = totalMeters * 0.000621371;
 
-        let totalSeconds = mapReduce(thisWeeksActivities, 'duration');
-        let d = moment.duration(totalSeconds, 'seconds');
-        let durationDisplay = d.hours() + 'h ' + d.minutes() + 'm';
+        let totalSeconds = mapReduce(summayActivities, 'duration');
+        const durationForDisplay = this.get('durationForDisplay');
+        let durationDisplay = durationForDisplay(totalSeconds);
 
         let summary = {
           distance: totalMiles.toFixed(2),
@@ -39,24 +45,39 @@ export default Ember.Route.extend({
         let thisWeeksActivities = activities.filter(function(value) {
           return moment.utc(value.start_time).isAfter(moment().day(-1));
         });
+
         //get summary for each day
         let summaries = [];
         let m = moment();
+
+        let dailyActivities = {};
+        for (var i = 0; i < thisWeeksActivities.length; i++) {
+          let activity = thisWeeksActivities[i];
+          let day = moment.utc(activity.start_time).day();
+          if (dailyActivities[day]) {
+            dailyActivities[day].push(activity);
+          } else {
+            dailyActivities[day] = [activity];
+          }
+        }
+        console.log(dailyActivities);
         for (var i = 0; i < 7; i++) {
-          let todaysActivities = thisWeeksActivities.filter(function(value){
-            return moment.utc(value.start_time).day() === m.day(i).day();
-          });
+          //need to move this out!
+          // let day = moment.utc(value.start_time).day();
+
+          let todaysActivities = dailyActivities[i] ? dailyActivities [i] : [];
 
           if (todaysActivities.length === 0) {
             let stat = {
               distance:0.0,
-              duration:'- h - m - s',
-              pace: '-:-/m',
+              duration:'rest day',
+              pace: '',
               title: m.day(i).format('dddd')
             };
-            summaries.push(stat);
-          } else {
 
+            summaries.push(stat);
+
+          } else {
             let totalMeters = mapReduce(todaysActivities, 'distance');
             let totalMiles = totalMeters * 0.000621371;
 
@@ -66,13 +87,25 @@ export default Ember.Route.extend({
 
             let pace = d.asMinutes()/totalMiles;
             let dd = moment.duration(pace, 'minutes');
-            let paceDisplay = dd.minutes() + ':' + dd.seconds() +"/m"
+            let paceDisplay = dd.minutes() + ':' + dd.seconds() +"/m";
 
+            var css= 'text-default';
+            if (totalMiles > 10) {
+              css = 'text-success';
+            } else if (totalMiles > 6 ) {
+              css = 'text-primary';
+            } else if (totalMiles > 0){
+              css = 'text-warning';
+            }
+
+            let title = m.day(i).format('dddd');
+            
             let stat = {
-              distance:totalMiles.toFixed(2),
-              duration:durationDisplay,
-              pace:paceDisplay,
-              title:m.day(i).format('dddd')
+              distance: totalMiles.toFixed(2),
+              duration: durationDisplay,
+              pace: paceDisplay,
+              title: title,
+              class: css
             };
 
             summaries.push(stat);
